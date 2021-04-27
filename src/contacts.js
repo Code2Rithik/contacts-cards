aebe9ef8e5f9e4edad014e2fa83fffa4fab529f5
Rithik Samanthula <talk2rithiks@gmail.com>
2021-04-27 14:54:14 +0530

    First Commit

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Darth Vader",
+    imgURL:
+      "https://i.pinimg.com/564x/01/f8/37/01f837a26650b7cf2ea5c20935104f61.jpg",
+    phone: "+123 456 789",
+    email: "dv@darhvader.com"
+  },
+  {
+    id: 2,
+    name: "Tony Stark",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1222654825403424768/-ySQePLc.jpg",
+    phone: "+987 654 321",
+    email: "im@ironman.com"
+  },
+  {
+    id: 3,
+    name: "Peter Parker",
+    imgURL:
+      "https://swall.teahub.io/photos/small/206-2068156_spider-man-circle-logo-spiderman-logo-clipart.jpg",
+    phone: "+918 372 574",
+    email: "spiderm@spiderman.com"
+  }
+];
+
+export default contacts;
